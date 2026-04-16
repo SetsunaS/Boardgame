99b3d953e47345903a97f58159d477a62b661119
SetsunaS <shana01112000@gmail.com>
2026-04-16 23:50:08 +0200

    fix label non updated locally

--- src/main/java/setsuna/boardgame/controller/TicTacToeController.java
+++ src/main/java/setsuna/boardgame/controller/TicTacToeController.java
@@ -292,6 +292,7 @@ public class TicTacToeController implements ControllerInterface, GameControllerI
                 }
                 showWinner();
             }
+            else updateCurrentPlayerName();
         }
     }
 
